daad9e631ae8df5217e96abaa7a227f711388281
Paul Borawski <paulborawski14@gmail.com>
2016-03-04 09:47:25 -0800

    finished how old you are calculator

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 #birthday calculator
 
-when you enter your birthday it will eventually say how old you are in minutes.
+This was a way to find out how old you are.
+This is NOT the easiest solution.
+The point of this exercises was to think outside the box.
+I turned dates and your birthday into decimals, subtracted the decimals, and then converted the decimals back into readable numbers
--- script.js
+++ script.js
@@ -1,20 +1,60 @@
-function getAge(yess) {
+/* creates a date and takes the year out of it as a number */
    var today = new Date();
-   var birthDate = new Date(yess);
-   var age = today.getFullYear() - birthDate.getFullYear();
-   var m = today.getMonth() - birthDate.getMonth();
-   if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-     age--;
-   }
-   return age;
+   var todayString = today.toString();
+   subStringYear = todayString.substr(11, 4);
+   subStringYearNum = Number(subStringYear);
+   subStringMonth = todayString.substr(4, 3);
+   subStringMonthNum = Number(subStringMonth);
+   subStringDay = todayString.substr(8, 2);
+
+//turns months into numerals
+    var months = ["nomonth", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+      for (var i = 0; i < months.length; i++){
+        if (subStringMonth === months[i]) {
+          var mounths = i;
+        }
+      }
+
+//turns month into two digit number if only a single digit number
+if (mounths.toString().length < 2) {
+  mounths = "0" + mounths;
+  Number(mounths);
 }
-
-var year = document.getElementById("year").value;
-console.log(year);
-var month = document.getElementById("month").value;
-console.log(month);
-var day = document.getElementById("day").value;
-console.log(day);
+var mounthsCorrect = (mounths / 12) / Math.pow(10, -2);
+var daysCorrect = (subStringDay / 30) / Math.pow(10, -2);
+var daysCorrect2 = Math.round(daysCorrect);
+var decimalDate = subStringYear + "." + mounthsCorrect + daysCorrect2;
 
 
-//document.write('age:' + getAge());
+function getAge(yess) {
+
+/* gets elements from html input field */
+
+  var one = document.getElementById("year").value;
+  var two = document.getElementById("month").value;
+  var three = document.getElementById("day").value;
+  var nombas = one + "." + two + three;
+  var myCorrect = (two / 12) / Math.pow(10, -2);
+  var myDaysCorrect = (three / 30) / Math.pow(10, -2);
+  var myDaysCorrect2 = Math.round(myDaysCorrect);
+  var decimalDates = one + "." + myCorrect + myDaysCorrect2;
+  var finalSolution = decimalDate - decimalDates;
+  var backToString = finalSolution.toString();
+  var yearZ = backToString.slice(0, 2);
+  var monthZ = backToString.slice(3, 5);
+  var dayZ = backToString.slice(5, 7);
+  var monthZ2 = Number((monthZ * 12) / Math.pow(10, 2));
+  var monthZ3 = monthZ2.toFixed(0);
+  var dayZ2 = Number((dayZ * 30) / Math.pow(10, 2));
+  var dayZ3 = dayZ2.toFixed(0);
+  document.write("You are " + yearZ + " years old" + "<br>");
+  document.write("You are " + monthZ3 + " months old" + "<br>");
+  document.write("You are " + dayZ3 + " days old" + "<br>");
+
+
+
+
+
+
+
+}
